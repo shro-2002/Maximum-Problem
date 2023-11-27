7c5c19d11028f56e12b61b61b89711a1ffded29e
shro-2002 <96790181+shro-2002@users.noreply.github.com>
2023-11-27 22:30:06 +0530

    Added UC-2

--- MaxGenerics.java
+++ MaxGenerics.java
@@ -4,14 +4,37 @@ public class MaxGenerics {
 	public static void main(String[] args) {
 		System.out.println("Welcome to the program to find the maximum number");
 
+		// Integer
+		
 		// Test Case 1
+		System.out.println("Test Case 1");
 		maxNumber(30, 20, 10);
+		System.out.println();
 
 		// Test Case 2
+		System.out.println("Test Case 2");
 		maxNumber(10, 50, 20);
+		System.out.println();
 
 		// Test Case 3
+		System.out.println("Test Case 3");
 		maxNumber(10, 20, 70);
+		System.out.println();
+
+		// Float
+
+		// Test Case 1
+
+		System.out.println("Test Case 1");
+		maxNumber1(15.5f, 10.5f, 5.5f);
+
+		// Test Case 2
+		System.out.println("Test Case 2");
+		maxNumber1(6.5f, 18.5f, 8.5f);
+
+		// Test Case 3
+		System.out.println("Test Case 3");
+		maxNumber1(9.5f, 12.5f, 34.5f);
 
 	}
 
@@ -25,6 +48,22 @@ public class MaxGenerics {
 
 	public static void maxNumber(Integer a, Integer b, Integer c) {
 		Integer max = a;
+		if (b.compareTo(max) > 0)
+			max = b;
+		if (c.compareTo(max) > 0)
+			max = c;
+		System.out.println("The maximum number is " + max);
+	}
+
+	/*
+	 * @params: Float , Float , Float
+	 * 
+	 * @return: void
+	 * 
+	 * @Description: This method takes 3 floats and finds the maximum among them
+	 */
+	public static void maxNumber1(Float a, Float b, Float c) {
+		Float max = a;
 		if (b.compareTo(max) > 0)
 			max = b;
 		if (c.compareTo(max) > 0)
